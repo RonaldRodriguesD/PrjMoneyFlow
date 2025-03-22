3baa92032d95c78df7c3338d4512a110f895b820
Ronald <ronalddantasrodrigues18@gmail.com>
2025-03-21 22:09:45 -0300

    criação dos models, controlers e migartions

---
+++ database/migrations/2025_03_22_003243_create_budgets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('budgets', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('limit',9,2);
+            $table->string('obs');
+            $table->timestamps();
+            $table->foreignId('category_id')->references('id')->on('categories');
+            $table->foreignId('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('budgets');
+    }
+};
