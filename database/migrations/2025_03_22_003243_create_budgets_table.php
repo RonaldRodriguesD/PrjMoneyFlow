d301ca37df57f5b46e95b644bc4758c98ad8510f
Ronald <ronalddantasrodrigues18@gmail.com>
2025-06-13 17:37:11 -0300

    alteração na tabela budgets, add de campo de data

--- database/migrations/2025_03_22_003243_create_budgets_table.php
+++ database/migrations/2025_03_22_003243_create_budgets_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('budgets', function (Blueprint $table) {
             $table->id();
             $table->decimal('limit',9,2);
+            $table->date('date');
             $table->string('obs');
             $table->timestamps();
             $table->foreignId('category_id')->references('id')->on('categories');
